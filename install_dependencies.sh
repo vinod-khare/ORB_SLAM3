ebc3ed793ca998ad98663d46d453a9497f8a8fc7
Vinod Khare <vinod.khare@icloud.com>
2026-05-28 13:01:46 -0600

    `cmake`: compile on Ubuntu-26.04

---
+++ install_dependencies.sh
@@ -0,0 +1,11 @@
+# install dependencies from apt
+sudo apt install cmake
+sudo apt install ninja-build
+sudo apt install libopencv-dev
+sudo apt install libeigen3-dev
+sudo apt install libcurl4-openssl-dev
+sudo apt install libboost-all-dev
+sudo apt install libspdlog-dev
+
+# install dependencies from vcpkg
+./.vcpkg/vcpkg install pangolin
