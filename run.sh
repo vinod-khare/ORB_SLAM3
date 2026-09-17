399cc10e2f5936e8267237dac0bf564925d99a7c
vinod-khare <vinod_khare@trimble.com>
2026-09-17 12:47:28 -0600

    `refactor`

--- run.sh
+++ run.sh
@@ -15,67 +15,16 @@ while [[ $# -gt 0 ]]; do
     *) shift ;;
   esac
 done
-BIN=".build/${BUILD_TYPE}/orbslam3"
+BIN=".build/${BUILD_TYPE}/orbslam3_app/orbslam3_app"
 
-# ==============================================================================
-# Option 1: Using positional arguments (original style)
-# ==============================================================================
 echo "Running orbslam3 on corridor1 dataset (positional arguments)..."
 $BIN \
   Vocabulary/ORBvoc.txt \
-  Examples/Monocular/TUM-VI.yaml \
+  config/TUM-VI.yaml \
   .data/tumvi/dataset-corridor1_512_16/mav0/cam0/data \
-  --slam-type mono-inertial \
+  --slam-type mono \
   --output-folder .data/tumvi/dataset-corridor1_512_16/orbslam3 \
   --frames-take 0 \
   --output trajectory \
   --save-ply
 
-# $BIN \
-#   Vocabulary/ORBvoc.txt \
-#   Examples/Monocular/reaper.yaml \
-#   .data/reaper/testoutdoor5/testoutdoor5 \
-#   .data/reaper/testoutdoor5/utc_timestamps.txt \
-#   --timestamps-type utc \
-#   --output-folder .data/reaper/testoutdoor5/orbslam3 \
-#   --frames-skip 0 \
-#   --frames-take 0 \
-#   --output trajectory \
-#   --save-ply
-
-## HILTI Dataset
-# $BIN \
-#   Vocabulary/ORBvoc.txt \
-#   Examples/Monocular/hilti.yaml \
-#   .data/hilti/2022/exp15_attic_to_upper_gallery/alphasense/cam0/image_raw \
-#   --output-folder .data/hilti/2022/exp15_attic_to_upper_gallery/orbslam3 \
-#   --frames-skip 0 \
-#   --frames-take 0 \
-#   --output trajectory \
-#   --save-ply
-
-# ==============================================================================
-# Option 2: Using named arguments
-# ==============================================================================
-# Uncomment to use named arguments instead:
-# echo "Running orbslam3 on corridor1 dataset (named arguments)..."
-# $BIN \
-#   --vocab Vocabulary/ORBvoc.txt \
-#   --settings Examples/Monocular/TUM-VI.yaml \
-#   --sequence ./.data/tumvi/dataset-corridor1_512_16/mav0/cam0/data \
-#   --sequence Examples/Monocular/TUM_TimeStamps/dataset-corridor1_512.txt \
-#   --output corridor1_trajectory
-
-# ==============================================================================
-# Option 3: Running on multiple sequences (corridor1 + corridor2)
-# ==============================================================================
-# Uncomment to run on both corridor sequences:
-# echo "Running orbslam3 on corridor1 + corridor2 datasets..."
-# $BIN \
-#   Vocabulary/ORBvoc.txt \
-#   Examples/Monocular/TUM-VI.yaml \
-#   .data/tumvi/dataset-corridor1_512_16/mav0/cam0/data \
-#   Examples/Monocular/TUM_TimeStamps/dataset-corridor1_512.txt \
-#   .data/tumvi/dataset-corridor2_512_16/mav0/cam0/data \
-#   Examples/Monocular/TUM_TimeStamps/dataset-corridor2_512.txt \
-#   --output corridor12_trajectory
